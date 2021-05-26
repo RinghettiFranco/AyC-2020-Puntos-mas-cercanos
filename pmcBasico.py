7772abe95bf4d620132905630d2ec70a06db7462
fran-gomez <fgvolonterio@gmail.com>
2021-05-26 15:10:52 -0300

    pmc Basico y optimizado. Funcion timer para 'benchmarking'

---
+++ pmcBasico.py
@@ -0,0 +1,46 @@
+from math import *
+from utils import *
+
+# Probamos todos con todos, cual orgia de Liberace (?)
+@timer_function
+def pmcBasico(Puntos):
+    min_dist = INF
+
+    for i in Puntos:
+        for j in Puntos:
+            if i != j:
+                m = min(min_dist, distancia(i, j))
+                if m < min_dist:
+                    pi = i
+                    pj = j
+                    min_dist = m
+
+    print(f'Distancia minima: {min_dist} entre los puntos {pi} y {pj}')
+
+    return min_dist
+
+# Optimizacion aplicada considerando la propiedad de
+# conmutatividad de la distancia. dist(x, y) = dist(y, x)
+@timer_function
+def pmcBasicoOptimizado(Puntos):
+    min_dist = INF
+
+    for i in range(0, len(Puntos)):
+        for j in range(i, len(Puntos)):
+            if i != j:
+                m = min(min_dist, distancia(Puntos[i], Puntos[j]))
+                if m < min_dist:
+                    pi = Puntos[i]
+                    pj = Puntos[j]
+                    min_dist = m
+    
+    print(f'Distancia minima: {min_dist} entre los puntos {pi} y {pj}')
+
+    return min_dist
+
+def test():
+    Puntos = generarPuntos(9) # Genero 2^9 puntos aleatorios
+    pmcBasico(Puntos)
+    pmcBasicoOptimizado(Puntos)
+
+test()
--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 from random import randint
 from math import sqrt
+import time
 
 MAX = 10**9
 INF = float('inf')
 
+# @brief Generador de puntos aleatorios
+# @param n Numero entero
+# @return Lista con 2^n puntos aleatorios
 def generarPuntos(n):
     puntos = list()
     for i in range(2**n):
-        x,y = randint(0,max), randint(0,max)
-        punto = (x,y)
+        x, y = randint(0, MAX), randint(0, MAX)
+        punto = (x, y)
         puntos.append(punto)
     return puntos
 
@@ -18,40 +22,73 @@ def ordenarPorX(puntos):
 def ordenarPorY(puntos):
     return puntos.sort(key = lambda y: y[1])
 
-def distancia(punto1,punto2):
+# @brief Calcula la distancia euclidea entre dos puntos
+# @param punto1 Punto del plano (x, y)
+# @param punto2 Punto del plano (x, y)
+# @return Distancia entre puntos
+def distancia(punto1, punto2):
     x1, y1 = punto1
     x2, y2 = punto2
     x = (x2-x1)**2
     y = (y2-y1)**2
     return sqrt(x+y)
 
-def aIzquierda(puntos,m):
+# @biref Toma los m primeros puntos del conjunto
+# @param puntos Conjunto de puntos
+# @param m Cota superior del intervalo
+# @return Conjunto de puntos en el intervalo [0, m)
+def aIzquierda(puntos, m):
     puntosAIzq = list()
-    for i in range(0,m):
+    for i in range(0, m):
         puntosAIzq.append(puntos[i])
     return puntosAIzq
 
-def aDerecha(puntos,m):
+# @biref Toma los ultimos m puntos del conjunto
+# @param puntos Conjunto de puntos
+# @param m Cota inferior del intervalo
+# @return Conjunto de puntos en el intervalo [m, max]
+def aDerecha(puntos, m):
     puntosADer = list()
-    for i in range(m,len(puntos)):
+    for i in range(m, len(puntos)):
         puntosADer.append(puntos[i])
     return puntosADer
 
-def crearFranja(puntos,m,d):
+# @brief Crea una franja en el plano que contiene puntos
+# @param puntos Conjunto de puntos
+# @param m Cota derecha de la franja
+# @param d Cota izquierda de la franja
+# @return Conjunto de puntos contenidos en la franja
+def crearFranja(puntos, m, d):
     franja = list()
     for i in puntos:
-       if puntos[i][0] in (m-d,m+d):
+       if puntos[i][0] in (m-d, m+d):
            franja.append(puntos[i][0])
     return franja 
 
-def recorrer(d,franja):
+# @brief Ni idea, este se los dejo
+# @param d
+# @param franja
+# @return 
+def recorrer(d, franja):
     for i in franja:
-        for j in (i+1,len(franja)):
+        for j in (i+1, len(franja)):
             ydiff = franja[j][1] - franja[i][1]
             if not ydiff > d:
-                dist = distancia(franja[j],franja[i])
+                dist = distancia(franja[j], franja[i])
                 if dist < d:
                     d = dist
     return d
     
 
+
+
+def timer_function(function):
+    def function_timer(*args, **kwargs):
+        start = time.time()
+        value = function(*args, **kwargs)
+        end = time.time()
+        runtime = end - start
+        msg = "{func} took {time} seconds to complete its execution."
+        print(msg.format(func = function.__name__,time = runtime))
+        return value
+    return function_timer
