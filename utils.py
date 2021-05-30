5c6f6a1e680ea2f3ab1746df8f8b1ff8b36bdde4
RinghettiFranco <54511049+RinghettiFranco@users.noreply.github.com>
2021-05-30 18:26:39 -0300

    Saque a la verga aIzquierda y aDerecha

--- utils.py
+++ utils.py
@@ -35,28 +35,6 @@ def distancia(punto1, punto2):
     y = (y2-y1)**2
     return sqrt(x+y)
 
-# @biref Toma los m primeros puntos del conjunto
-# @param puntos Conjunto de puntos
-# @param m Cota superior del intervalo
-# @return Conjunto de puntos en el intervalo [0, m)
-def aIzquierda(puntos, m):
-    puntosAIzq = list()
-    for punto in puntos: 
-        if punto[0] < m:
-            puntosAIzq.append(punto)
-    return puntosAIzq
-
-# @biref Toma los ultimos m puntos del conjunto
-# @param puntos Conjunto de puntos
-# @param m Cota inferior del intervalo
-# @return Conjunto de puntos en el intervalo [m, max]
-def aDerecha(puntos, m):
-    puntosADer = list()
-    for punto in puntos:
-        if punto[0] >= m:
-            puntosADer.append(punto)
-    return puntosADer
-
 # @brief Crea una franja en el plano que contiene puntos
 # @param puntos Conjunto de puntos
 # @param m Cota derecha de la franja
