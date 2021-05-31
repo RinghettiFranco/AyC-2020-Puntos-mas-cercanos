dc869ebe0ef84d54f9b669da55e8ff63b0969bf0
fran-gomez <fgvolonterio@gmail.com>
2021-05-31 00:02:20 -0300

    Tester incluido

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
+from multiprocessing import Process, current_process
+
 from utils import *
 from pmcDyCAvanzado import *
 from pmcBasico import *
 from pmcDyC import *
 
-puntos = []
-print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
-for n in range(1,14):
-    puntos = generarPuntos(n)
-    print('Para n = ' + str(n) + ' tal que #puntos=2^n=2^' + str(n) + '=' + str(2**n)  + ':')
-    print()
-    r1 = pmcBasico(puntos)
-    print('Resultado: ' + str(r1[0]) + ' entre los puntos ' + str(r1[1]) + ' y ' + str(r1[2]))
-    print()
-    r2 = pmcBasicoOptimizado(puntos)
-    print('Resultado: ' + str(r2[0]) + ' entre los puntos ' + str(r2[1]) + ' y ' + str(r2[2]))
-    print()
-    r3 = pmcDyCSimple(puntos)
-    print('Resultado: ' + str(r3[0]) + ' entre los puntos ' + str(r3[1]) + ' y ' + str(r3[2]))
-    print()
-    r4 = pmcDyCAvanzado(puntos)
-    print('Resultado: ' + str(r4[0]) + ' entre los puntos ' + str(r4[1]) + ' y ' + str(r4[2]))
-    print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------') 
-for n in range(14,21):
-    puntos = generarPuntos(n)
-    print('Para n = ' + str(n) + ' tal que #puntos=2^n=2^' + str(n) + '=' + str(2**n)  + ':')
-    print()
-    r3 = pmcDyCSimple(puntos)
-    print('Resultado: ' + str(r3[0]) + ' entre los puntos ' + str(r3[1]) + ' y ' + str(r3[2]))
-    print()
-    r4 = pmcDyCAvanzado(puntos)
-    print('Resultado: ' + str(r4[0]) + ' entre los puntos ' + str(r4[1]) + ' y ' + str(r4[2]))
-    print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+if __name__ == '__main__':
+    problemas = 0
+    f = open("debug.txt", "w")
+    for w in range(0, 100):
+        puntos = []
+        print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+        for n in range(1,14):
+            puntos = generarPuntos(n)
+
+            print(f'Para n = {n} tal que #puntos=2^n=2^{n}={2**n}:\n')
+            r1 = pmcBasico(puntos)
+            print(f'Resultado: {r1[0]} entre los puntos {r1[1]} y {r1[2]}\n')
+            r2 = pmcBasicoOptimizado(puntos)
+            print(f'Resultado: {r2[0]} entre los puntos {r2[1]} y {r2[2]}\n')
+            r3 = pmcDyCSimple(puntos)
+            print(f'Resultado: {r3[0]} entre los puntos {r3[1]} y {r3[2]}\n')
+            r4 = pmcDyCAvanzado(puntos)
+            print(f'Resultado: {r4[0]} entre los puntos {r4[1]} y {r4[2]}\n')
+
+            if r3[0] != r4[0]:
+                problemas += 1
+                f.write(f'Problema con {r3} y {r4} en la vuelta {w} con n={n}')
+            print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------') 
+        for n in range(14,21):
+            puntos = generarPuntos(n)
+            print(f'Para n = {n} tal que #puntos=2^n=2^{n}={2**n}:\n')
+            r3 = pmcDyCSimple(puntos)
+            print(f'Resultado: {r3[0]} entre los puntos {r3[1]} y {r3[2]}\n')
+            r4 = pmcDyCAvanzado(puntos)
+            print(f'Resultado: {r4[0]} entre los puntos {r4[1]} y {r4[2]}\n')
+
+            if r3[0] != r4[0]:
+                problemas += 1
+                f.write(f'Problema con {r3} y {r4} en la vuelta {w} con n={n}\n')
+            print('------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print(f'Iteracion {w} - Hubo {problemas} problemas\n')
+    f.close()
